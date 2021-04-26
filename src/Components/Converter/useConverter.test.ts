0c0bc67eaf0e7cc26556d5bd68679f89b17a61be
RomanKh <roman.rs.kh@gmail.com>
2021-04-26 21:55:49 +0200

    test: created useConverter todo

---
+++ src/Components/Converter/useConverter.test.ts
@@ -0,0 +1,20 @@
+import { useConverter } from "./useConverter";
+
+describe("when rendered", () => {
+  it.todo("Rub input should have a value equal to initial Rub amount");
+  it.todo("Usd input should have a value equal to calculated Usd amount");
+});
+
+describe("when call an 'updateRub' method", () => {
+  it.todo("should update the Rub value");
+  it.todo("should recalculate the Usd value");
+});
+
+describe("when call an 'updateUsd' method", () => {
+  it.todo("should update the Usd value");
+  it.todo("should recalculate the Rub value");
+});
+
+describe("when re-render", () => {
+  it.todo("should update its values");
+});
