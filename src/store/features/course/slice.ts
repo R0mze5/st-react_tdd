df90948add814aec7a915627dd882ea2e7c81946
RomanKh <roman.rs.kh@gmail.com>
2021-04-28 00:07:21 +0200

    add async fetch course action

--- src/store/features/course/slice.ts
+++ src/store/features/course/slice.ts
@@ -1,13 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchCourse } from "../../../api";
 
 export const initialCourseState = {
   value: 42,
+  status: null,
 };
+
+export const fetchTodayCourse = createAsyncThunk(
+  "course/fetchCourse",
+  async () => {
+    const response = await fetchCourse();
+    return response.course;
+  }
+);
 
 const courseSlice = createSlice({
   name: "course",
   initialState: initialCourseState,
   reducers: {},
+  extraReducers: {
+    [fetchTodayCourse.pending.toString()]: (state, action) => ({ ...state }),
+    [fetchTodayCourse.fulfilled.toString()]: (state, action) => ({ ...state }),
+    [fetchTodayCourse.rejected.toString()]: (state, action) => ({ ...state }),
+  },
 });
 
 export default courseSlice.reducer;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-// eslint-disable-next-line import/no-cycle
+
 import course from "./features/course/slice";
 
 export const store = configureStore({
