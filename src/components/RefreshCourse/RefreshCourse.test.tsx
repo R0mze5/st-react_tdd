5a3d91752452f11ec309a6b7b297d909d4707d52
RomanKh <roman.rs.kh@gmail.com>
2021-04-28 20:43:32 +0200

    store: added reset status action

--- src/components/RefreshCourse/RefreshCourse.test.tsx
+++ src/components/RefreshCourse/RefreshCourse.test.tsx
@@ -1,5 +1,9 @@
 import { RefreshCourse } from "./RefreshCourse";
 
+describe("when the button in default state", () => {
+  it.todo("it should contain dafault state");
+});
+
 describe("when Refresh button click", () => {
   it.todo("should request a fresh course");
 });
--- src/components/RefreshCourse/RefreshCourse.tsx
+++ src/components/RefreshCourse/RefreshCourse.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import styles from "./RefreshCourse.module.css";
 
 export const RefreshCourse: React.FC = () => {
+  const dispatch = useDispatch();
+
   return (
     <button className={styles.button} type="button" onClick={console.log}>
       refresh course
